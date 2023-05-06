dfeb5d07891accfd9ca178b9b4e62235f9f86c9a
arthur <art.webcode@gmail.com>
2023-05-06 14:23:55 +0600

    Finish block composition

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppHeader from "../AppHeader/AppHeader";
 import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
 import data from '../../utils/data'
+import style from './App.module.css'
 
 function App() {
     return (
@@ -10,7 +11,7 @@ function App() {
             <main>
                 <div className="container">
                     <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
-                    <div className="content">
+                    <div className={style.content}>
                         <BurgerIngredients data={data}/>
                     </div>
                 </div>
--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,69 +1,184 @@
 import React from 'react';
-import {Tab} from '@ya.praktikum/react-developer-burger-ui-components'
+import {Tab, ConstructorElement, DragIcon, CurrencyIcon, Button} from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './BurgerIngredients.module.css'
 import Ingredient from "../Ingredient/Ingredient";
 
 function BurgerIngredients({data}) {
     const [current, setCurrent] = React.useState('one')
+    const imgBun = 'https://code.s3.yandex.net/react/code/bun-02.png';
 
     return (
-        <div className={styles.leftSide}>
-            <div className={`${styles.menu} mb-10`}>
-                <Tab value="one" active={current === 'one'} onClick={setCurrent}>
-                    <a className={styles.tabLink} href="#section1">
-                        <p className="text text_type_main-default">
+        <>
+            <div className={styles.leftSide}>
+                <div className={`${styles.menu} mb-10`}>
+                    <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+                        <a className={styles.tabLink} href="#section1">
+                            <p className="text text_type_main-default">
+                                Булки
+                            </p>
+                        </a>
+                    </Tab>
+                    <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+                        <a className={styles.tabLink} href="#section2">
+                            <p className="text text_type_main-default">
+                                Соусы
+                            </p>
+                        </a>
+                    </Tab>
+                    <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+                        <a className={styles.tabLink} href="#section3">
+                            <p className="text text_type_main-default">
+                                Начинки
+                            </p>
+                        </a>
+                    </Tab>
+                </div>
+                <div className={`${styles.list} custom-scroll`}>
+                    <section id="section1" className={styles.section}>
+                        <p className="text text_type_main-medium mb-6">
                             Булки
                         </p>
-                    </a>
-                </Tab>
-                <Tab value="two" active={current === 'two'} onClick={setCurrent}>
-                    <a className={styles.tabLink} href="#section2">
-                        <p className="text text_type_main-default">
+                        <div className={styles.row}>
+                            {data.filter(el => el.type === 'bun').map((newEl) => {
+                                return <Ingredient {...newEl} key={newEl.id}/>
+                            })}
+                        </div>
+                    </section>
+                    <section id="section2" className={styles.section}>
+                        <p className="text text_type_main-medium mb-6">
                             Соусы
                         </p>
-                    </a>
-                </Tab>
-                <Tab value="three" active={current === 'three'} onClick={setCurrent}>
-                    <a className={styles.tabLink} href="#section3">
-                        <p className="text text_type_main-default">
+                        <div className={styles.row}>
+                            {data.filter(el => el.type === 'sauce').map((newEl) => {
+                                return <Ingredient {...newEl} key={newEl.id}/>
+                            })}
+                        </div>
+                    </section>
+                    <section id="section3" className={styles.section}>
+                        <p className="text text_type_main-medium mb-6">
                             Начинки
                         </p>
-                    </a>
-                </Tab>
+                        <div className={styles.row}>
+                            {data.filter(el => el.type === 'main').map((newEl) => {
+                                return <Ingredient {...newEl} key={newEl.id}/>
+                            })}
+                        </div>
+                    </section>
+                </div>
             </div>
-            <div className={`${styles.list} custom-scroll`}>
-                <section id="section1" className={styles.section}>
-                    <p className="text text_type_main-medium mb-6">
-                        Булки
-                    </p>
-                    <div className={styles.row}>
-                        {data.filter(el => el.type === 'bun').map((newEl) => {
-                            return <Ingredient {...newEl} key={newEl.id}/>
-                        })}
+            <div className={styles.rightSide}>
+                <div className={styles.composition}>
+                    <div className={styles.composition__border}>
+                        <ConstructorElement
+                            type="top"
+                            isLocked={true}
+                            text="Краторная булка N-200i (верх)"
+                            price={200}
+                            thumbnail={imgBun}
+                        />
                     </div>
-                </section>
-                <section id="section2" className={styles.section}>
-                    <p className="text text_type_main-medium mb-6">
-                        Соусы
-                    </p>
-                    <div className={styles.row}>
-                        {data.filter(el => el.type === 'sauce').map((newEl) => {
-                            return <Ingredient {...newEl} key={newEl.id}/>
-                        })}
+                    <div className={`${styles.composition__list} custom-scroll`}>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Соус традиционный галактический"
+                                price={30}
+                                thumbnail="https://code.s3.yandex.net/react/code/sauce-03.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Мясо бессмертных моллюсков Protostomia"
+                                price={300}
+                                thumbnail="https://code.s3.yandex.net/react/code/meat-02.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Плоды Фалленианского дерева"
+                                price={80}
+                                thumbnail="https://code.s3.yandex.net/react/code/sp_1.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Хрустящие минеральные кольца"
+                                price={80}
+                                thumbnail="https://code.s3.yandex.net/react/code/mineral_rings.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Хрустящие минеральные кольца"
+                                price={80}
+                                thumbnail="https://code.s3.yandex.net/react/code/mineral_rings.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Соус традиционный галактический"
+                                price={30}
+                                thumbnail="https://code.s3.yandex.net/react/code/sauce-03.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Мясо бессмертных моллюсков Protostomia"
+                                price={300}
+                                thumbnail="https://code.s3.yandex.net/react/code/meat-02.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Плоды Фалленианского дерева"
+                                price={80}
+                                thumbnail="https://code.s3.yandex.net/react/code/sp_1.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Хрустящие минеральные кольца"
+                                price={80}
+                                thumbnail="https://code.s3.yandex.net/react/code/mineral_rings.png"
+                            />
+                        </div>
+                        <div className={styles.composition__row}>
+                            <DragIcon type="primary" />
+                            <ConstructorElement
+                                text="Хрустящие минеральные кольца"
+                                price={80}
+                                thumbnail="https://code.s3.yandex.net/react/code/mineral_rings.png"
+                            />
+                        </div>
                     </div>
-                </section>
-                <section id="section3" className={styles.section}>
-                    <p className="text text_type_main-medium mb-6">
-                        Начинки
-                    </p>
-                    <div className={styles.row}>
-                        {data.filter(el => el.type === 'main').map((newEl) => {
-                            return <Ingredient {...newEl} key={newEl.id}/>
-                        })}
+                    <div className={styles.composition__border}>
+                        <ConstructorElement
+                            type="bottom"
+                            isLocked={true}
+                            text="Краторная булка N-200i (низ)"
+                            price={200}
+                            thumbnail={imgBun}
+                        />
                     </div>
-                </section>
+                </div>
+                <div className={`${styles.check} mt-10`}>
+                    <p className="text text_type_digits-medium mr-2">610</p>
+                    <CurrencyIcon type="primary"/>
+                    <Button htmlType="button" type="primary" size="medium">
+                        Оформить заказ
+                    </Button>
+                </div>
             </div>
-        </div>
+        </>
+
     );
 }
 
