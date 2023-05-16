c3f59df269d0a91fe1af9d92e90495ca83c28ca7
arthur <art.webcode@gmail.com>
2023-05-16 18:07:23 +0600

    Submit for review: sprint-2/step-1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,8 @@ import style from './App.module.css'
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 import OrderDetails from "../OrderDetails/OrderDetails";
 import IngredientDetails from "../IngredientDetails/IngredientDetails";
-import {getIngredients} from '../../utils/burger-api'
+import {getIngredients, sendOrder} from '../../utils/burger-api'
+import {BurgerConstructorContext, TotalPriceContext} from "../../contexts/BurgerConstructorContext";
 
 
 function App() {
@@ -24,9 +25,15 @@ function App() {
         }
     )
 
+    const [checkID, setCheckID] = useState('0000')
+
+    const [totalPrice, setTotalPrice] = useState(0)
+
+    const [order, setOrder] = useState([]);
+
     function handleOpenModal(ingredient = null, type = 'ingredient') {
         if (type === 'check') {
-
+            sendOrder().then(data => setCheckID(data.order.number))
         }
 
         if(type === 'ingredient') {
@@ -43,38 +50,65 @@ function App() {
         setStatePopup({type: '', visible: false})
     }
 
+    function calcTotalPrice() {
+        let price = 0;
+        if(order.length > 1) {
+            order.forEach(el => {
+                price += el.price;
+            })
+        }
+        setTotalPrice(price);
+    }
+
+    function fakeOrder() {
+        if(state.ingredients.length > 0 && !state.hasError && !state.isLoading) {
+            const bun = state.ingredients.find((item) => item.type === 'bun');
+            const orderContent = state.ingredients.filter(item => item.type !== 'bun');
+            setOrder([bun, ...orderContent]);
+            calcTotalPrice();
+        }
+    }
+
     const getProducts = () => {
-        getIngredients().then(data => setState({...state, ingredients: data.data, isLoading: false}))
+        getIngredients().then(data => setState({...state, ingredients: data.data, isLoading: false}));
     };
 
     useEffect(() => {
         getProducts();
     }, [])
 
+    useEffect(() => {
+        fakeOrder();
+    }, [state])
+
     return (
         <div className="App">
             <AppHeader/>
             <main>
                 <div className="container">
-                    {statePopup.visible && statePopup.type === 'check' &&
-                        <OrderDetails onClose={handleCloseModal}/>
-                    }
-                    {statePopup.visible && statePopup.type === 'ingredient' &&
-                        <IngredientDetails onClose={handleCloseModal} ingredient={selectIngredient}/>
-                    }
+                    <BurgerConstructorContext.Provider value={order}>
+                        <TotalPriceContext.Provider value={{totalPrice, calcTotalPrice}}>
+                            {statePopup.visible && statePopup.type === 'check' &&
+                                <OrderDetails onClose={handleCloseModal} checkID={checkID}/>
+                            }
+                            {statePopup.visible && statePopup.type === 'ingredient' &&
+                                <IngredientDetails onClose={handleCloseModal} ingredient={selectIngredient}/>
+                            }
 
-                    <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
-                    <div className={style.content}>
-                        {state.isLoading && 'Загрузка...'}
-                        {state.hasError && 'Произошла ошибка'}
-                        {state.ingredients.length > 0 && !state.hasError && !state.isLoading && (
-                            <>
-                                <BurgerIngredients data={state.ingredients} openModal={handleOpenModal}/>
-                                <BurgerConstructor openModal={handleOpenModal}/>
-                            </>
-                        )
-                        }
-                    </div>
+                            <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
+                            <div className={style.content}>
+                                {state.isLoading && 'Загрузка...'}
+                                {state.hasError && 'Произошла ошибка'}
+                                {state.ingredients.length > 0 && !state.hasError && !state.isLoading && (
+                                    <>
+                                        <BurgerIngredients data={state.ingredients} openModal={handleOpenModal}/>
+                                        <BurgerConstructor openModal={handleOpenModal}/>
+                                    </>
+                                )
+                                }
+                            </div>
+                        </TotalPriceContext.Provider>
+                    </BurgerConstructorContext.Provider>
                 </div>
             </main>
         </div>
--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -4,6 +4,25 @@ export const getIngredients = () => {
     return fetch(`${NORMA_API}/ingredients`).then(checkReponse)
 }
 
+export const sendOrder = async () => {
+    try {
+        const response = await fetch(`${NORMA_API}/orders`, {
+            method: 'POST',
+            body: JSON.stringify({
+                "ingredients": ["643d69a5c3f7b9001cfa093c", "643d69a5c3f7b9001cfa093e"]
+            } ),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const json = await response.json();
+        console.log('Успех:', JSON.stringify(json));
+        return json;
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+}
+
 const checkReponse = (res) => {
     return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
